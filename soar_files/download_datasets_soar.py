c37cb43bcb9976069bb6cac1b5a7ddfe64d4a73a
Elliott Fogg <elliott.fogg@gmail.com>
2024-04-13 17:11:18 +0100

    Cleaned and updated files for transfer to WSL

---
+++ soar_files/download_datasets_soar.py
@@ -0,0 +1,14 @@
+import sys
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib
+import numpy as np
+import astropy.io.fits as pyfits
+import astropy.utils as autils
+import requests
+import json
+from pprint import pprint as pp  # pretty print
+import requests, sys, json, os, datetime
+from urllib import urlencode
+from os.path import dirname, abspath
+from os.path import join as pathjoin
